403a700a4cad9e4d23d69e394a9009b6fc2d22de
Gregor von Laszewski <laszewski@gmail.com>
2022-07-17 08:07:32 -0400

    add expect script

--- cloudmesh/vpn/vpn.py
+++ cloudmesh/vpn/vpn.py
@@ -46,6 +46,17 @@ from cloudmesh.common.util import writefile
 class Vpn:
 
     def __init__(self, service=None, debug=False):
+
+        if os_is_windows():
+            self.anyconnect = r'C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpncli.exe'
+        elif os_is_mac():
+            self.anyconnect = "/opt/cisco/anyconnect/bin/vpn"
+        elif os_is_linux():
+            self.anyconnect = "/opt/cisco/anyconnect/bin/vpn"
+        else:
+            raise NotImplementedError("OS is not yet supported for any connect")
+
+
         self.debug = debug
         if service is None or service == "uva":
             self.service = "UVA Anywhere"
@@ -80,12 +91,8 @@ class Vpn:
         if os_is_windows():
             result = requests.get("ipinfo.io")
             state = "University of Virginia" in result["org"]
-        elif os_is_mac():
-            command = f'/opt/cisco/anyconnect/bin/vpn'
-            result = Shell.run(command)
-            state = "virginia.edu" in result
-        elif os_is_linux():
-            command = f'/opt/cisco/anyconnect/bin/vpn'
+        elif os_is_mac() or os_is_linux():
+            command = self.anyconnect
             result = Shell.run(command)
             state = "virginia.edu" in result
         self._debug(result)
@@ -93,7 +100,7 @@ class Vpn:
 
     def connect(self):
         if os_is_windows():
-            mycommand = r'C:\Program Files (x86)\Cisco\Cisco AnyConnect Secure Mobility Client\vpncli.exe connect "UVA Anywhere'
+            mycommand = rf'{self.anyconnect} connect "UVA Anywhere'
             # mycommand = mycommand.replace("\\", "/")
             r = pexpect.popen_spawn.PopenSpawn(mycommand)
             sys.stdout.reconfigure(encoding='utf-8')
@@ -101,13 +108,19 @@ class Vpn:
             #time.sleep(5)
             #r.sendline('y')
             status = False
-            result = r.expect([pexpect.TIMEOUT, r"^.*accept.*$", r"^.*Another AnyConnect application.*$", pexpect.EOF])
+            result = r.expect([pexpect.TIMEOUT,
+                               r"^.*accept.*$",
+                               r"^.*Another AnyConnect application.*$",
+                               pexpect.EOF])
             if result == 2:
-                Console.error("Please run in administrative git bash 'net stop vpnagent' and 'net start vpnagent'")
+                Console.error("Please run in administrative git bash "
+                              "'net stop vpnagent' and 'net start vpnagent'")
                 return status
             if result == 1:
                 r.sendline('y')
-                result2 = r.expect([pexpect.TIMEOUT, r"^.*Connected.*$", pexpect.EOF])
+                result2 = r.expect()
+                print (result2)
+                result2 = r.expect([pexpect.TIMEOUT, "^.*Connected.*$", pexpect.EOF])
                 if result2 == 1:
                     Console.ok('Successfully connected')
                     status = True
@@ -120,8 +133,6 @@ class Vpn:
             result = Shell.run("expect /tmp/connect-uva.exp")
             Shell.rm("/tmp/connect-uva.exp")
 
-            #command = f'yes | /opt/cisco/anyconnect/bin/vpn connect "{self.service}"'
-            #result = Shell.run(command)
         elif os_is_linux():
 
             connect = readfile(pkg_resources.resource_filename(__name__, 'etc/connect-uva.exp'))
