bc1c5ccdb3fb2779519b967f3fb11a1f32f43e47
J.P <70083705+stapmoshun@users.noreply.github.com>
2023-10-26 03:44:55 -0400

    towards macOS functionality

--- cloudmesh/vpn/windows.py
+++ cloudmesh/vpn/windows.py
@@ -1,58 +1,88 @@
 from cloudmesh.common.Shell import Shell
 from cloudmesh.common.Shell import Console
+from cloudmesh.common.systeminfo import os_is_linux
+from cloudmesh.common.systeminfo import os_is_mac
+from cloudmesh.common.systeminfo import os_is_windows
 import os
 import subprocess
 
 def win_install():
     
+    if os_is_windows:
+        # Get the full path of the current Python script
+        current_script_path = os.path.abspath(__file__)
 
-    # Get the full path of the current Python script
-    current_script_path = os.path.abspath(__file__)
+        # Extract the directory path from the script's path
+        current_script_directory = os.path.dirname(current_script_path)
 
-    # Extract the directory path from the script's path
-    current_script_directory = os.path.dirname(current_script_path)
+        status = Shell.install_chocolatey()
+        if status is False:
+            os._exit(1)
+        Console.msg('Installing cisco...')
+        # try:
+        #     r =
+        # except subprocess.CalledProcessError as e:
 
-    status = Shell.install_chocolatey()
-    if status is False:
-        os._exit(1)
-    Console.msg('Installing cisco...')
-    # try:
-    #     r =
-    # except subprocess.CalledProcessError as e:
+        command = f'cd {current_script_directory}/bin && choco install chocolatey-core.extension -y && choco pack && choco install cisco-secure-client --debug --verbose --source . --force -y'
 
-    command = f'cd {current_script_directory}/bin && choco install chocolatey-core.extension -y && choco pack && choco install cisco-secure-client --debug --verbose --source . --force -y'
+        # try:
 
-    # try:
+        #     r = Shell.run()
+        # except subprocess.CalledProcessError as e:
+        #     print(e.output)
 
-    #     r = Shell.run()
-    # except subprocess.CalledProcessError as e:
-    #     print(e.output)
-
-    process = subprocess.Popen(
-        command,
-        shell=True,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        universal_newlines=True  # Allows working with text output
-    )
-
-    # Read and display the live output
-    for line in process.stdout:
-        print(line, end="")
-
-    # Wait for the subprocess to complete
-    process.wait()
-
-    # print(r)
-    try:
         process = subprocess.Popen(
-            'vpncli',
+            command,
             shell=True,
             stdout=subprocess.PIPE,
             stderr=subprocess.PIPE,
             universal_newlines=True  # Allows working with text output
         )
-        print("success")
-    except subprocess.CalledProcessError:
-        print("failed")
+
+        # Read and display the live output
+        for line in process.stdout:
+            print(line, end="")
+
+        # Wait for the subprocess to complete
+        process.wait()
+
+        # print(r)
+        try:
+            process = subprocess.Popen(
+                'vpncli',
+                shell=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                universal_newlines=True  # Allows working with text output
+            )
+            print("success")
+        except subprocess.CalledProcessError:
+            print("failed")
+
+
+    elif os_is_mac:
+        status = Shell.install_brew()
+        if status is False:
+            os._exit(1)
+
+        Console.msg('Installing cisco...')
+        # try:
+        #     r =
+        # except subprocess.CalledProcessError as e:
+
+        command = f'cd {current_script_directory}/bin && choco install chocolatey-core.extension -y && choco pack && choco install cisco-secure-client --debug --verbose --source . --force -y'
+
+        # try:
+
+        #     r = Shell.run()
+        # except subprocess.CalledProcessError as e:
+        #     print(e.output)
+
+        process = subprocess.Popen(
+            command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            universal_newlines=True  # Allows working with text output
+        )
 
