ea85ea7c0111809950f6a994a747429c921ec149
J.P <70083705+stapmoshun@users.noreply.github.com>
2023-10-27 03:17:35 -0400

    cisco cask

--- cloudmesh/vpn/bin/cisco-secure-client.rb
+++ cloudmesh/vpn/bin/cisco-secure-client.rb
@@ -2,12 +2,12 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 cask "cisco-secure-client" do
-  desc ""
-  homepage ""
+  desc "cisco client"
+  homepage "https://www.cisco.com/site/us/en/products/security/secure-client/index.html"
   url "https://olemiss.edu/helpdesk/vpn/_files/cisco-secure-client-macos-5.0.01242-predeploy-k9.dmg"
   sha256 "54ce96a427efad22c755332f6b56fc3559e811a3add15cb3485e5e409bb20aaf"
-  license ""
 
+  app "Cisco Secure Client.app"
   # depends_on "cmake" => :build
 
 
