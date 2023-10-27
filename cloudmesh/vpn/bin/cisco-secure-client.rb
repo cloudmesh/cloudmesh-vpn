5467b6366ebf095b8e12e4772761c40621eccb99
J.P <70083705+stapmoshun@users.noreply.github.com>
2023-10-27 03:08:37 -0400

    cisco formulae

--- cloudmesh/vpn/bin/cisco-secure-client.rb
+++ cloudmesh/vpn/bin/cisco-secure-client.rb
@@ -1,7 +1,7 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class CiscoSecureClient < Formula
+cask "cisco-secure-client" do
   desc ""
   homepage ""
   url "https://olemiss.edu/helpdesk/vpn/_files/cisco-secure-client-macos-5.0.01242-predeploy-k9.dmg"
@@ -10,24 +10,6 @@ class CiscoSecureClient < Formula
 
   # depends_on "cmake" => :build
 
-  def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test cisco-secure-client`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+  
   end
 end
